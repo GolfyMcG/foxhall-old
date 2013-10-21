e49f00592f6c8303aa0ad769aaed0abaa987472a
Alex Villa <avilla0429@gmail.com>
2013-10-21 16:58:00 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foxhall::Application.config.secret_key_base = '0bd5e2b961c4ece885bd0a0ed707e823508f6aeae74e7ee07db94e54a9e61cbade7989b5a30e2585d7b154500285687631d384b60d0d82bfe562ca9db75abf2f'
