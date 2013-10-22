28c0d4fbe1fc8640a4e7e1166ac832d6cd28842b
Alex Villa <avilla0429@gmail.com>
2013-10-22 01:49:56 -0400

    Adds new fonts, logo, and home page js scrolling

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+  // **********************************
+  // ** ScrollTo Function for header **
+  // **********************************
+  $('a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      var target = this.hash,
+      $target = $(target);
+      $('html, body').stop().animate(
+        { 'scrollTop': $target.offset().top },
+        800,
+        'swing',
+        function() { window.location.hash = target; }
+      );
+  });
+
+  // **********************************
+  // ** Lock header to top on scroll **
+  // **********************************
+  var docked = false;
+  var menu = $('nav');
+  var navHeight = menu.height();
+  var heroHeight = $('#hero').height();
+  var placeholder = $("#nav-placeholder");
+  placeholder.height(navHeight);
+  var init = menu.offset().top;
+
+  $(window).scroll(function() {
+    if (!docked && (menu.offset().top - $("body").scrollTop() < 0)) {
+      menu.addClass("fixed");
+      placeholder.removeClass("fixed");
+      docked = true;
+    }
+    else if(docked && $("body").scrollTop() <= init) {
+      menu.removeClass("fixed");
+      placeholder.addClass("fixed");
+      docked = false;
+    }
+    if ($("body").scrollTop() + navHeight >= heroHeight) {
+      $('.wrapper article').each(function(i) {
+        if ($(this).position().top <= $("body").scrollTop() - navHeight) {
+            $('nav li.active').removeClass('active');
+            $('nav li').eq(i+1).addClass('active');
+        }
+      });
+    } else {
+      $('nav li.active').removeClass('active');
+      $('nav li:first').addClass('active');
+    }
+  });
+
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,23 @@ module ApplicationHelper
   def print_page_id
     return "id="+"page-"+params[:controller]+"-"+params[:action]
   end
+
+  def is_home?
+    if params[:controller] == "pages" && params[:action] == "home"
+      return true
+    else
+      return false
+    end
+  end
+
+  def nav_link(link_name, div_name, options = { })
+    href = options[:href]
+    if params[:controller] == "pages" and params[:action] =="home"
+      href ||= "##{h div_name}"
+    else
+      href ||= "#{h root_path}##{h div_name}"
+    end
+    options[:href] = href
+    content_tag(:li, content_tag(:a, link_name, options))
+  end
 end
